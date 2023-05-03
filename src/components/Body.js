877e74db3a076c9469559f26cbadc61c916a888c
Amar Nath <nthvpsn@gmail.com>
2023-05-03 16:27:10 +0530

    added search function

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,42 @@
 import RestaurantCard from "./RestuarantCard";
 import { restaurantList } from "../config";
+import { useState } from "react";
+
 const BodyComponent = () => {
+  //making a react state variable of restuarant list
+  const [restuarants, setRestaurants] = useState(restaurantList);
+  const [searchText, setSearchText] = useState("");
+
+  function filterData(searchText, restuarants) {
+    return restuarants.filter((restaurant) => restaurant.data.name.includes(searchText));
+  }
   return (
-    <div className="cards">
-      {restaurantList.map((restaurant) => {
-        return (
-          <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-        );
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-box"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button className="search-btn" onClick={() => {
+          const data = filterData(searchText, restuarants);
+          setRestaurants(data);
+        }}>
+          Search
+        </button>
+      </div>
+      <div className="cards">
+        {restuarants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
